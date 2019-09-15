d48c2ebb1c5abad095d8cec6393bfccd83f19db8
Barak22 <barakm22@gmail.com>
2019-09-15 23:31:33 +0300

    Added input file and get the local file to the browser

--- src/main/scala/com/example/whatsapp/frontend/js/index.js
+++ src/main/scala/com/example/whatsapp/frontend/js/index.js
@@ -1,19 +1,35 @@
-(async () => {
-  const response = await fetch("http://localhost:8080/conversation");
-  const json = await
-    response.json();
+// (async () => {
+//   const response = await fetch("http://localhost:8080/conversation");
+//   const json = await
+//     response.json();
+//
+//   Object.keys(json.messages).forEach(k => {
+//     createDateComponent(k);
+//     json.messages[k].forEach(createMessageComponent)
+//   });
+// })();
+//
+// function createMessageComponent(message) {
+//   const date = new Date(message.date);
+//   document.querySelector('.col-8').innerHTML += `<div>${date.getHours()}:${date.getMinutes()} : ${message.from} : ${message.content}</div>`
+// }
+//
+// createDateComponent = (date) => {
+//   document.querySelector('.col-8').innerHTML += `<div>${date}</div>`
+// };
+//
 
-  Object.keys(json.messages).forEach(k => {
-    createDateComponent(k);
-    json.messages[k].forEach(createMessageComponent)
-  });
-})();
+// function uploadButtonOnClick() {
+//   document.getElementById('input-file').click()
+// }
 
-function createMessageComponent(message) {
-  const date = new Date(message.date);
-  document.querySelector('.col-8').innerHTML += `<div>${date.getHours()}:${date.getMinutes()} : ${message.from} : ${message.content}</div>`
+function inputFileOnChange(file) {
+  const path = document.getElementById('input-file').files[0];
+  const reader = new FileReader();
+  console.log(path);
+  reader.readAsBinaryString(path);
+  reader.onloadend = e => {
+    console.log(e.target.result); // TODO: Send the zip file to the scala server
+  }
+
 }
-
-createDateComponent = (date) => {
-  document.querySelector('.col-8').innerHTML += `<div>${date}</div>`
-};
